dae4e05b56837a33aa4aca691874f6ee380ff5fd
Martin Gutierrez <mart.gutierrezm@duocuc.cl>
2026-09-24 20:23:08 -0300

    Ajustes finales en GestorFonda

--- src/main/java/cl/dsy1102/fonda/GestorFonda.java
+++ src/main/java/cl/dsy1102/fonda/GestorFonda.java
@@ -4,70 +4,80 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GestorFonda {
-    List<Bebida> bebidas;
+
+    private List<Bebida> bebidas;
 
     public GestorFonda() {
         bebidas = new ArrayList<>();
     }
 
     public void registrar(Bebida bebida) {
-        this.bebidas.add(bebida);
-        System.out.println(bebida.getNombre() + "-" + bebida.getClass().getSimpleName());
-        System.out.println("Se ha registrado correctamente");
+        bebidas.add(bebida);
+
+        System.out.println(bebida.getNombre() + "-" + bebida.getClass().getSimpleName() + ")" + " registrada correctamente.");
     }
 
     public List<Bebida> buscarPorNombre(String nombre) {
+
         List<Bebida> encontradas = new ArrayList<>();
 
         for (Bebida bebida : bebidas) {
+
             if (bebida.getNombre().equalsIgnoreCase(nombre)) {
                 encontradas.add(bebida);
             }
-
-
         }
-        return encontradas;
 
+        return encontradas;
     }
 
     public void vender(String nombre, int unidades) {
-        Bebida bebidaencontrada = null;
+
+        Bebida bebidaEncontrada = null;
 
         for (Bebida bebida : bebidas) {
-            if (bebida.getNombre().equalsIgnoreCase(nombre)) ;
-            bebidaencontrada = bebida;
-            break;
+
+            if (bebida.getNombre().equalsIgnoreCase(nombre)) {
+                bebidaEncontrada = bebida;
+                break;
+            }
         }
-        if (bebidaencontrada == null) {
-            System.out.println("no ha sido encontrada: " + nombre);
 
-        }
-        if (bebidaencontrada instanceof ConsumoResponsable) {
-            ConsumoResponsable control = (ConsumoResponsable) bebidaencontrada;
+        if (bebidaEncontrada == null) {
+            System.out.println("No se encontró la bebida: " + nombre);
 
-
-            if (control.tieneVentaRestringida()) ;
-            System.out.println("Venta rechazada: " + bebidaencontrada.getNombre());
-
-
-            if (control.superarLimite(unidades));
-            System.out.println("Venta rechazada" + unidades + "-" + bebidaencontrada.getNombre() + "Supera a las : " + ConsumoResponsable.LIMITE_DE_UNIDADES + "AL CLIENTE");
 
         }
-        double total = bebidaencontrada.calcularPrecio() * unidades;
-        System.out.println("Ventas: " + unidades + "-" + bebidaencontrada.getNombre() + "Total" + total);
 
+        if (bebidaEncontrada instanceof ConsumoResponsable) {
+
+            ConsumoResponsable control =
+                    (ConsumoResponsable) bebidaEncontrada;
+
+            if (control.tieneVentaRestringida()) {
+
+                System.out.println("Venta rechazada:" + bebidaEncontrada.getNombre() + "Tiene la venta restringida");
+
+
+            }
+
+            if (control.superarLimite(unidades)) {
+
+                System.out.println( "Venta rechazada: " + unidades + "-" + bebidaEncontrada.getNombre() + "Supera a las: " + ConsumoResponsable.LIMITE_DE_UNIDADES + "Al cliente");
+
+
+
+            }
+        }
+
+        double total = bebidaEncontrada.calcularPrecio() * unidades;
+
+        System.out.println("Venta autorizada: " + unidades + " x " + bebidaEncontrada.getNombre() + "| Total: $" + total);
 
     }
-    public List<Bebida> obtenerTodas(){
+
+    public List<Bebida> obtenerTodas() {
         return bebidas;
     }
-
-
 }
-
-
-
-
-
 
